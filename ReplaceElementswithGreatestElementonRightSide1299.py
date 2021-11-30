cd44f3c46a509265823acc546f86a801a5fece41
Sri Ram <sv2333@nyu.edu>
2021-11-30 15:01:04 -0500

    Add files via upload

---
+++ ReplaceElementswithGreatestElementonRightSide1299.py
@@ -0,0 +1,10 @@
+class Solution:
+    def replaceElements(arr):
+        curMax = -1
+        for i in range(len(arr)-1, -1, -1):
+            arr[i], curMax = curMax, max(arr[i], curMax)
+        return arr
+
+
+
+        
