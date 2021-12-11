a1f47866ee8c398b8d1a5a94c43434ab64b072f3
Sri Ram <sv2333@nyu.edu>
2021-12-11 15:50:11 -0500

    Add files via upload

---
+++ SortArrayByParity905.py
@@ -0,0 +1,24 @@
+class Solution:
+    def sortArrayByParity(self, nums):
+        left = 0
+        right = len(nums)-1
+        while left < right:
+            if nums[left] %2 != 0 and nums[right] % 2 == 0:
+                nums[right], nums[left] = nums[left], nums[right]
+                left += 1
+                right -= 1
+            elif nums[left] %2 == 0 and nums[right]%2 != 0:
+                left += 1
+                right -= 1
+            elif nums[left]%2 == 0:
+                left += 1
+            else:
+                right -= 1
+        return nums
+
+
+
+
+
+
+        
