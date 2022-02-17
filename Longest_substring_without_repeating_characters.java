d15cc43a2d02038355c153e48e3664922e52ab8a
Sri Ram <sv2333@nyu.edu>
2022-02-16 23:05:47 -0500

    Create Longest_substring_without_repeating_characters.java

---
+++ Longest_substring_without_repeating_characters.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        int i=0,j=0;
+        int max=0;
+        HashSet<Character> hash_set = new HashSet();
+        
+        while(j<s.length()){
+            if(!hash_set.contains(s.charAt(j))){
+                hash_set.add(s.charAt(j));
+                j++;
+                max=Math.max(max,hash_set.size());  
+            }
+            else{
+                hash_set.remove(s.charAt(i));
+                i++;
+            }
+        }
+        return max;
+    }
+}
