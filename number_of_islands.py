fdb2fbc4e1b7cab2a3cbad4e232d2c8736f248ce
Sri Ram <sv2333@nyu.edu>
2022-02-19 14:19:45 -0500

    Create number_of_islands.py

---
+++ number_of_islands.py
@@ -0,0 +1,25 @@
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        visit=set()
+        islands=0
+        rows=len(grid)
+        cols=len(grid[0])
+        def bfs(r,c):
+            q=collections.deque()
+            visit.add((r,c))
+            q.append((r,c))
+            while q:
+                row,col=q.popleft()
+                directions=[[1,0],[-1,0],[0,1],[0,-1]]
+                for dr,dc in directions:
+                    r,c = row+dr,col+dc
+                    if(r in range(rows) and c in range(cols) and grid[r][c]=='1' and (r,c) not in visit):
+                        q.append((r,c))
+                        visit.add((r,c))
+            
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] =='1' and (r,c) not in visit:
+                    bfs(r,c)
+                    islands+=1
+        return islands
