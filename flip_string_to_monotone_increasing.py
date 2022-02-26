51645f9bb2e335080985ffb75a96393bca2e2e6e
Sri Ram <sv2333@nyu.edu>
2022-02-26 12:27:07 -0500

    Create flip_string_to_monotone_increasing.py

---
+++ flip_string_to_monotone_increasing.py
@@ -0,0 +1,11 @@
+class Solution:
+    def minFlipsMonoIncr(self, s: str) -> int:
+        zero,one=s.count('0'),0
+        output=zero
+        for d in s:
+            if d=='0':
+                zero-=1
+            if d=='1':
+                one+=1
+            output=min(zero+one,output)
+        return output
