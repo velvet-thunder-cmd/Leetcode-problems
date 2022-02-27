fa92a6b760f92ca063f871d8c2bacefefd9c817e
Sri Ram <sv2333@nyu.edu>
2022-02-27 16:56:36 -0500

    Create island_perimeter.py

---
+++ island_perimeter.py
@@ -0,0 +1,22 @@
+class Solution:
+    visit=set()
+    def islandPerimeter(self, grid: List[List[int]]) -> int:
+        visit=set()
+        def dfs(i,j):
+            if i>=len(grid) or j>=len(grid[0]) or \
+            i<0 or j<0 or grid[i][j]==0:
+                return 1
+            if (i,j) in visit:
+                return 0
+            
+            visit.add((i,j))
+            peri=dfs(i+1,j)
+            peri+=dfs(i,j+1)
+            peri+=dfs(i-1,j)
+            peri+=dfs(i,j-1)
+            return peri
+        
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j]:
+                    return dfs(i,j)
